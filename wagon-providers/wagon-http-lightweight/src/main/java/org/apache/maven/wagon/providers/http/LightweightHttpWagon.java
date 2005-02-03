81185494e7c6876a577c6d5cb6ed213d22e8d049
Brett Leslie Porter <brett@apache.org>
2005-02-03 11:39:15 +0000

    o add basic auth to model and lightweight wagon o add proxy configuration in lightweight wagon o not yet wiring in the properties from maven.properties into the repository
    git-svn-id: https://svn.apache.org/repos/asf/maven/wagon/trunk@162414 13f79535-47bb-0310-9956-ffa450edef68

--- wagon-providers/wagon-http-lightweight/src/main/java/org/apache/maven/wagon/providers/http/LightweightHttpWagon.java
+++ wagon-providers/wagon-http-lightweight/src/main/java/org/apache/maven/wagon/providers/http/LightweightHttpWagon.java
@@ -16,16 +16,22 @@ package org.apache.maven.wagon.providers.http;
  * limitations under the License.
  */
 
-import org.apache.maven.wagon.*;
-import org.apache.maven.wagon.resource.Resource;
+import org.apache.maven.wagon.ConnectionException;
+import org.apache.maven.wagon.InputData;
+import org.apache.maven.wagon.OutputData;
+import org.apache.maven.wagon.ResourceDoesNotExistException;
+import org.apache.maven.wagon.StreamWagon;
+import org.apache.maven.wagon.TransferFailedException;
 import org.apache.maven.wagon.authentication.AuthenticationException;
+import org.apache.maven.wagon.proxy.ProxyInfo;
 import org.apache.maven.wagon.repository.Repository;
+import org.apache.maven.wagon.resource.Resource;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.net.Authenticator;
 import java.net.MalformedURLException;
+import java.net.PasswordAuthentication;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -33,21 +39,22 @@ import java.net.URLConnection;
  * @author <a href="michal.maczka@dimatics.com">Michal Maczka</a>
  * @version $Id$
  */
-public class LightweightHttpWagon extends StreamWagon
+public class LightweightHttpWagon
+extends StreamWagon
 {
+    private String previousProxyExclusions;
+    private String previousProxyHost;
+    private String previousProxyPort;
 
-    public void fillInputData( InputData inputData ) throws TransferFailedException, ResourceDoesNotExistException
+    public void fillInputData( InputData inputData )
+    throws TransferFailedException, ResourceDoesNotExistException
     {
         Repository repository = getRepository();
-
         String repositoryUrl = repository.getUrl();
-
         Resource resource = inputData.getResource();
-
         try
-        {
+              {
             URL url;
-
             if ( repositoryUrl.endsWith( "/" ) )
             {
                 url = new URL( repositoryUrl + resource.getName() );
@@ -56,44 +63,64 @@ public class LightweightHttpWagon extends StreamWagon
             {
                 url = new URL( repositoryUrl + "/" + resource.getName() );
             }
-
             URLConnection urlConnection = url.openConnection();
-
             inputData.setInputStream( urlConnection.getInputStream() );
-
             resource.setLastModified( urlConnection.getLastModified() );
-
             resource.setContentLength( urlConnection.getContentLength() );
         }
         catch ( MalformedURLException e )
         {
             throw new ResourceDoesNotExistException( e.getMessage() );
-
         }
-        catch( FileNotFoundException e )
+        catch ( FileNotFoundException e )
         {
-           throw new ResourceDoesNotExistException( e.getMessage() );
+            throw new ResourceDoesNotExistException( e.getMessage() );
         }
         catch ( IOException e )
-       {
+        {
             throw new TransferFailedException( e.getMessage() );
         }
-
-
     }
 
-    public void fillOutputData( OutputData outputData ) throws TransferFailedException
+    public void fillOutputData( OutputData outputData )
+    throws TransferFailedException
     {
-        throw new  UnsupportedOperationException( "PUT operation is not supported by Light Weight  HTTP wagon" );
+        throw new UnsupportedOperationException( "PUT operation is not supported by Light Weight  HTTP wagon" );
     }
 
-    public void openConnection() throws ConnectionException, AuthenticationException
+    public void openConnection()
+    throws ConnectionException, AuthenticationException
     {
-
+        previousProxyHost = System.getProperty( "http.proxyHost" );
+        previousProxyPort = System.getProperty( "http.proxyPort" );
+        previousProxyExclusions = System.getProperty( "http.nonProxyHosts" );
+        final ProxyInfo proxyInfo = this.getRepository().getProxyInfo();
+        System.setProperty( "http.proxyHost", proxyInfo.getHost() );
+        System.setProperty( "http.proxyPort", String.valueOf( proxyInfo.getPort() ) );
+        System.setProperty( "http.nonProxyHosts", proxyInfo.getNonProxyHosts() );
+        if ( proxyInfo.getUserName() != null )
+        {
+            Authenticator.setDefault( new Authenticator()
+            {
+                protected PasswordAuthentication getPasswordAuthentication()
+                {
+                    String password = "";
+                    if ( proxyInfo.getPassword() != null )
+                    {
+                        password = proxyInfo.getPassword();
+                    }
+                    return new PasswordAuthentication( proxyInfo.getUserName(), password.toCharArray() );
+                }
+            } );
+        }
     }
 
-    public void closeConnection() throws ConnectionException
+    public void closeConnection()
+    throws ConnectionException
     {
-
+        System.setProperty( "http.proxyHost", previousProxyHost );
+        System.setProperty( "http.proxyPort", previousProxyPort );
+        System.setProperty( "http.nonProxyHosts", previousProxyExclusions );
     }
 }
+
