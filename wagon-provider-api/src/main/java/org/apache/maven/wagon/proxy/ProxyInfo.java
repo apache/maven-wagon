4f819fedec3a0019709b29adefadb2aefad8314a
Brett Leslie Porter <brett@apache.org>
2005-02-09 09:54:23 +0000

    add NTLM settings and reformat
    git-svn-id: https://svn.apache.org/repos/asf/maven/wagon/trunk@162424 13f79535-47bb-0310-9956-ffa450edef68

--- wagon-provider-api/src/main/java/org/apache/maven/wagon/proxy/ProxyInfo.java
+++ wagon-provider-api/src/main/java/org/apache/maven/wagon/proxy/ProxyInfo.java
@@ -31,121 +31,137 @@ import java.io.Serializable;
  * which can be shared between wagon api and providers
  */
 public class ProxyInfo
-implements Serializable
+    implements Serializable
 {
     public final static String PROXY_SOCKS5 = "SOCKS_5";
     public final static String PROXY_SOCKS4 = "SOCKS4";
     public final static String PROXY_HTTP = "HTTP";
+
     /**
-         * Proxy server host
-         */
+     * Proxy server host
+     */
     private String host = null;
+
     /**
-         * Username used to access the proxy server
-         */
+     * Username used to access the proxy server
+     */
     private String userName = null;
+
     /**
-         * Password associated with the proxy server
-         */
+     * Password associated with the proxy server
+     */
     private String password = null;
+
     /**
-         * Proxy server port
-         */
+     * Proxy server port
+     */
     private int port = WagonConstants.UNKNOWN_PORT;
+
     /**
-         * Type of the proxy
-         */
+     * Type of the proxy
+     */
     private String type = null;
+
     /**
-         * The non-proxy hosts. Follows Java system property format: <code>*.foo.com|localhost</code>.
-         */
+     * The non-proxy hosts. Follows Java system property format: <code>*.foo.com|localhost</code>.
+     */
     private String nonProxyHosts;
 
     /**
-         * Return proxy server host name.
-         *
-         * @return proxy server host name
-         */
+     * For NTLM proxies, specifies the NTLM host.
+     */
+    private String ntlmHost;
+
+    /**
+     * For NTLM proxies, specifies the NTLM domain.
+     */
+    private String ntlmDomain;
+
+    /**
+     * Return proxy server host name.
+     *
+     * @return proxy server host name
+     */
     public String getHost()
     {
         return host;
     }
 
     /**
-         * Set proxy host name.
-         *
-         * @param host proxy server host name
-         */
+     * Set proxy host name.
+     *
+     * @param host proxy server host name
+     */
     public void setHost( final String host )
     {
         this.host = host;
     }
 
     /**
-         * Get user's password used to login to proxy server.
-         *
-         * @return user's password at proxy host
-         */
+     * Get user's password used to login to proxy server.
+     *
+     * @return user's password at proxy host
+     */
     public String getPassword()
     {
         return password;
     }
 
     /**
-         * Set the user's password for the proxy server.
-         *
-         * @param password password to use to login to a proxy server
-         */
+     * Set the user's password for the proxy server.
+     *
+     * @param password password to use to login to a proxy server
+     */
     public void setPassword( final String password )
     {
         this.password = password;
     }
 
     /**
-         * Get the proxy port.
-         *
-         * @return proxy server port
-         */
+     * Get the proxy port.
+     *
+     * @return proxy server port
+     */
     public int getPort()
     {
         return port;
     }
 
     /**
-         * Set the proxy port.
-         *
-         * @param port proxy server port
-         */
+     * Set the proxy port.
+     *
+     * @param port proxy server port
+     */
     public void setPort( final int port )
     {
         this.port = port;
     }
 
     /**
-         * Get the proxy username.
-         *
-         * @return username for the proxy server
-         */
+     * Get the proxy username.
+     *
+     * @return username for the proxy server
+     */
     public String getUserName()
     {
         return userName;
     }
 
     /**
-         * Set the proxy username.
-         *
-         * @param userName username for the proxy server
-         */
+     * Set the proxy username.
+     *
+     * @param userName username for the proxy server
+     */
     public void setUserName( final String userName )
     {
         this.userName = userName;
     }
 
     /**
-         * Get the type of the proxy server.
-         *
-         * @return the type of the proxy server
-         */
+     * Get the type of the proxy server.
+     *
+     * @return the type of the proxy server
+     */
     public String getType()
     {
         return type;
@@ -167,5 +183,25 @@ implements Serializable
     public void setNonProxyHosts( String nonProxyHosts )
     {
         this.nonProxyHosts = nonProxyHosts;
+    }
+
+    public String getNtlmHost()
+    {
+        return ntlmHost;
+    }
+
+    public void setNtlmHost( String ntlmHost )
+    {
+        this.ntlmHost = ntlmHost;
+    }
+
+    public void setNtlmDomain( String ntlmDomain )
+    {
+        this.ntlmDomain = ntlmDomain;
+    }
+
+    public String getNtlmDomain()
+    {
+        return ntlmDomain;
     }
 }
