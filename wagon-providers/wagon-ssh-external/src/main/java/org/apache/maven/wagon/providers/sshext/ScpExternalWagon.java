c875026744ae72ea2ea0433a6e641baa4591b8ca
Brett Leslie Porter <brett@apache.org>
2005-06-01 03:07:06 +0000

    PR: MPARTIFACT-46 add -f to chmod/chgrp, it may be valid
    git-svn-id: https://svn.apache.org/repos/asf/maven/wagon/trunk@179342 13f79535-47bb-0310-9956-ffa450edef68

--- wagon-providers/wagon-ssh-external/src/main/java/org/apache/maven/wagon/providers/sshext/ScpExternalWagon.java
+++ wagon-providers/wagon-ssh-external/src/main/java/org/apache/maven/wagon/providers/sshext/ScpExternalWagon.java
@@ -264,12 +264,12 @@ public class ScpExternalWagon
 
         if ( permissions != null && permissions.getGroup() != null )
         {
-            executeCommand( "chgrp " + permissions.getGroup() + " " + basedir + "/" + resourceName + "\n" );
+            executeCommand( "chgrp -f " + permissions.getGroup() + " " + basedir + "/" + resourceName + "\n" );
         }
 
         if ( permissions != null && permissions.getFileMode() != null )
         {
-            executeCommand( "chmod " + permissions.getFileMode() + " " + basedir + "/" + resourceName + "\n" );
+            executeCommand( "chmod -f " + permissions.getFileMode() + " " + basedir + "/" + resourceName + "\n" );
         }
         firePutCompleted( resource, source );
     }
