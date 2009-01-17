a80d74b7a968e9508c18f3b830ee05f9145a1053
Benjamin Bentmann <bentmann@apache.org>
2009-01-17 12:28:00 +0000

    o Made test more robust with regard to timer resolution
    git-svn-id: https://svn.apache.org/repos/asf/maven/wagon/trunk@735280 13f79535-47bb-0310-9956-ffa450edef68

--- wagon-providers/wagon-ssh-common/src/test/java/org/apache/maven/wagon/providers/ssh/knownhost/FileKnownHostsProviderTest.java
+++ wagon-providers/wagon-ssh-common/src/test/java/org/apache/maven/wagon/providers/ssh/knownhost/FileKnownHostsProviderTest.java
@@ -42,7 +42,8 @@ public class FileKnownHostsProviderTest
         testKnownHostsFile = new File( basedir, "target/known_hosts" );
         testKnownHostsFile.delete();
         FileUtils.copyFile( readonlyKnownHostFile, testKnownHostsFile );
-       
+        testKnownHostsFile.setLastModified( testKnownHostsFile.lastModified() - 60 * 1000 );
+
         provider = new FileKnownHostsProvider( testKnownHostsFile );
        
     }
@@ -55,7 +56,6 @@ public class FileKnownHostsProviderTest
         File sameKnownHostFile = new File( basedir, "src/test/resources/known_hosts_same" );
         String contents = FileUtils.fileRead( sameKnownHostFile );
         
-        Thread.sleep( 50 );
         provider.storeKnownHosts( contents );
         assertEquals( "known_hosts file is rewritten", timestamp, testKnownHostsFile.lastModified() );
     }
@@ -68,7 +68,6 @@ public class FileKnownHostsProviderTest
         String contents = FileUtils.fileRead( sameKnownHostFile );
         contents += "1 2 3";
         
-        Thread.sleep( 50 );
         provider.storeKnownHosts( contents );
         assertTrue( "known_hosts file is not rewritten", timestamp != testKnownHostsFile.lastModified() );
     }
