f3d31e09a7463f3ad3500a62dcf36ce0bb0ddfda
Benjamin Bentmann <bentmann@apache.org>
2009-11-18 11:02:04 +0000

    o Added license header
    git-svn-id: https://svn.apache.org/repos/asf/maven/wagon/trunk@881718 13f79535-47bb-0310-9956-ffa450edef68

--- wagon-tcks/wagon-tck-http/src/main/java/org/apache/maven/wagon/tck/http/HttpWagonTests.java
+++ wagon-tcks/wagon-tck-http/src/main/java/org/apache/maven/wagon/tck/http/HttpWagonTests.java
@@ -1,5 +1,24 @@
 package org.apache.maven.wagon.tck.http;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import static org.apache.maven.wagon.tck.http.util.TestUtil.getResource;
 
 import org.apache.maven.wagon.ConnectionException;
@@ -63,8 +82,8 @@ public abstract class HttpWagonTests
         System.setProperty( "javax.net.ssl.trustStorePassword", ServerFixture.SERVER_SSL_KEYSTORE_PASSWORD );
 
         container = new DefaultPlexusContainer();
-        container.initialize();
-        container.start();
+        //container.initialize();
+        //container.start();
 
         configurator = (WagonTestCaseConfigurator) container.lookup( WagonTestCaseConfigurator.class.getName() );
     }
