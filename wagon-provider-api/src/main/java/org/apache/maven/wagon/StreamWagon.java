164194f2dea6ea14e016f2451d7ccc8f2cf4529a
zhangh <zhangh@dtdream.com>
2016-07-29 16:06:26 +0800

    update checkstyle

--- wagon-provider-api/src/main/java/org/apache/maven/wagon/StreamWagon.java
+++ wagon-provider-api/src/main/java/org/apache/maven/wagon/StreamWagon.java
@@ -89,7 +89,7 @@ public abstract class StreamWagon
 
         if ( new StreamWagonEx( this ).handle( timestamp, resource, destination ) )
         {
-            IOUtil.close(is);
+            IOUtil.close( is );
 
             return true;
         }
